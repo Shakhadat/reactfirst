b65a89cd6a8786d9dffeb9123c8dd9ccced60895
shahodat <Shahodatbonu95@mail.ru>
2022-01-29 11:27:29 +0500

    Reactni o'zimiz tuzamiz

--- src/index.js
+++ src/index.js
@@ -51,8 +51,8 @@ const container=document.getElementById("root");
 ReactDOM.render(element, container);
 
 */
-// JSX siz React.createElement bn yozish
-
+// JSX siz React.createElement bn yozish ichma-ich;
+/*
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -63,3 +63,74 @@ const element=React.createElement("div",{id:'foo'},
 
 const container=document.getElementById("root");
 ReactDOM.render(element,container);
+*/
+// yangittan logikani yaratish
+
+
+/** @jsxRuntime classic */
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            // title: "title",
+            // id: "id",
+            // className: "name",
+            children: children.map(child =>
+                typeof child === "object" ? child : createTextElement(child)
+            )
+        }
+    };
+}
+
+
+// <h1 title="title" id="id" className="name">children</h1>
+
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text, // hello
+            children: []
+        }
+    };
+}
+
+function render(element, container) {
+    const dom =
+        element.type == "TEXT_ELEMENT"
+            ? document.createTextNode("")
+            : document.createElement(element.type); // h1
+
+    const isProperty = key => key !== "children";
+
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name];
+        });
+
+    element.props.children.forEach(child => render(child, dom));
+    container.appendChild(dom);
+}
+
+const Act = {
+    createElement,
+    render
+};
+
+/** @jsx Act.createElement */
+const element = (
+    <div style="background: salmon">
+        <h1>Hello World</h1>
+        <h2 style="text-align:right">from Act</h2>
+        <h3>hello world</h3>
+        <ul>
+            <li><a><span style="color: red">code</span></a></li>
+        </ul>
+    </div>
+);
+const container = document.getElementById("root");
+Act.render(element, container);
+
+
